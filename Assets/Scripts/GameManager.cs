c3b4d7aaa8467bf94bcd868c5e5c042527e1bdd4
Austin Keppers <akeppers21@gmail.com>
2020-04-18 15:33:36 -0400

    Added typing and basic correctness visuals

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float drainInterval = 1f;
     [SerializeField] private ScreenManager screen;
     private IEnumerator drainRoutine;
+    private string currentTextInput;
+    private string correctText = "The quick brown fox jumps\nover the lazy dog";
+    private List<int> correctnessList = new List<int>();
+    private int correctIndex;
 
     public float Power
     {
@@ -21,13 +25,41 @@ public class GameManager : MonoBehaviour
     {
         drainRoutine = DrainBattery(drainAmount, drainInterval);
         StartCoroutine(drainRoutine);
+        currentTextInput = "";
+        correctIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        screen.UpdateScreenText("The quick brown fox jumps\n over the lazy dog", new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-        ,1,1,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (currentTextInput.Length != 0)
+                {
+                    currentTextInput = currentTextInput.Substring(0, currentTextInput.Length - 1);
+                    correctnessList.RemoveAt(correctnessList.Count - 1);
+                    correctIndex--;
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                currentTextInput = "";
+                correctnessList = new List<int>();
+                correctIndex = 0;
+            }
+            else
+            {
+                currentTextInput += c;
+                if(correctIndex < correctText.Length && c == correctText[correctIndex])
+                    correctnessList.Add(1);
+                else
+                    correctnessList.Add(2);
+                correctIndex++;
+            }
+        }
+        screen.UpdateScreenText(correctText, correctnessList.ToArray());
     }
 
     private IEnumerator DrainBattery(float amount, float interval)

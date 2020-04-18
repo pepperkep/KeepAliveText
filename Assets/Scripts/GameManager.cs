55d69acf2c86d4a9476895c61e512390dc21de93
Austin Keppers <akeppers21@gmail.com>
2020-04-18 16:20:36 -0400

    Typing increases battery and text swaps

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float power = 100f;
-    [SerializeField] private float drainAmount = 0.05f;
+    [SerializeField] private AnimationCurve drainAmount;
     [SerializeField] private float drainInterval = 1f;
+    [SerializeField] private float correctnessToEnergyConversion = 0.2f;
     [SerializeField] private ScreenManager screen;
     private IEnumerator drainRoutine;
     private string currentTextInput;
+    private List<string> texts = new List<string>();
+    private int textsIndex = 0;
     private string correctText = "The quick brown fox jumps\nover the lazy dog";
     private List<int> correctnessList = new List<int>();
     private int correctIndex;
@@ -25,6 +28,8 @@ public class GameManager : MonoBehaviour
     {
         drainRoutine = DrainBattery(drainAmount, drainInterval);
         StartCoroutine(drainRoutine);
+        texts.Add("The quick brown fox jumps\nover the lazy dog");
+        texts.Add("Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua.");
         currentTextInput = "";
         correctIndex = 0;
     }
@@ -45,14 +50,18 @@ public class GameManager : MonoBehaviour
             }
             else if ((c == '\n') || (c == '\r')) // enter/return
             {
+                AddBattery(correctnessList);
                 currentTextInput = "";
                 correctnessList = new List<int>();
                 correctIndex = 0;
+                textsIndex = (textsIndex + 1) % texts.Count;
+                correctText = texts[textsIndex];    
             }
             else
             {
                 currentTextInput += c;
-                if(correctIndex < correctText.Length && c == correctText[correctIndex])
+                if(correctIndex < correctText.Length && (c == correctText[correctIndex] ||
+                    (c == ' ' && correctText[correctIndex] == '\n')))
                     correctnessList.Add(1);
                 else
                     correctnessList.Add(2);
@@ -62,12 +71,28 @@ public class GameManager : MonoBehaviour
         screen.UpdateScreenText(correctText, correctnessList.ToArray());
     }
 
-    private IEnumerator DrainBattery(float amount, float interval)
+    private void AddBattery(List<int> correctness)
     {
-        while(power > 0)
+        float addedEnergy = 0;
+        for(int i = 0; i < correctness.Count; i++)
+        {
+            addedEnergy += correctness[i];
+        }
+        addedEnergy /= correctnessToEnergyConversion;
+        if(Power + addedEnergy > 100)
+            Power = 100;
+        else
+            Power += addedEnergy;
+    }
+
+    private IEnumerator DrainBattery(AnimationCurve amount, float interval)
+    {
+        float totalTime = 0;
+        while(Power > 0)
         {
             yield return new WaitForSeconds(interval);
-            Power -= amount;
+            totalTime += interval;
+            Power -= amount.Evaluate(totalTime);
             screen.SetBatteryText(power);
         }
     }

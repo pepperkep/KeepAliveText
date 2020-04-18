12faade248641aff54f0f14564f471b480809c49
Austin Keppers <akeppers21@gmail.com>
2020-04-18 12:11:58 -0400

    Added screen and updating battery value

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScreenManager : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI batteryText;
+
+    public void SetBatteryText(float batteryValue)
+    {
+        float roundedValue = Mathf.Round(batteryValue * 10) / 10;
+        string nextBatteryText = "" + roundedValue;
+        if(roundedValue % 1 == 0)
+            nextBatteryText += ".0";
+        nextBatteryText += "%";
+        batteryText.text = nextBatteryText;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float power = 100f;
+    [SerializeField] private float drainAmount = 0.05f;
+    [SerializeField] private float drainInterval = 1f;
+    [SerializeField] private ScreenManager screen;
+    private IEnumerator drainRoutine;
+
+    public float Power
+    {
+        get => power;
+        set => power = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drainRoutine = DrainBattery(drainAmount, drainInterval);
+        StartCoroutine(drainRoutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator DrainBattery(float amount, float interval)
+    {
+        while(power > 0)
+        {
+            yield return new WaitForSeconds(interval);
+            Power -= amount;
+            screen.SetBatteryText(power);
+        }
+    }
+}

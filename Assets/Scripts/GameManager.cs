eef1d73ab05b1a5f4eb98ff14cdb5f34137a9e51
Austin Keppers <akeppers21@gmail.com>
2020-04-18 19:40:27 -0400

    Added method to alter characters in text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float drainInterval = 1f;
     [SerializeField] private float correctnessToEnergyConversion = 0.2f;
     [SerializeField] private ScreenManager screen;
+    [SerializeField] private List<char> glitchingCharacters = new List<char>();
+    [SerializeField] private int minGlitchAmount = 1;
+    [SerializeField] private int maxGlitchAmount = 3;
     private IEnumerator drainRoutine;
     private string currentTextInput;
     private List<string> texts = new List<string>();
     private int textsIndex = 0;
     private string correctText = "The quick brown fox jumps\nover the lazy dog";
+    private string glitchedText;
     private List<int> correctnessList = new List<int>();
     private int correctIndex;
 
@@ -32,7 +36,9 @@ public class GameManager : MonoBehaviour
         texts.Add("Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua.");
         currentTextInput = "";
         correctIndex = 0;
+        glitchedText = correctText;
         screen.UpdateScreenText(correctText, correctnessList.ToArray(), Power); 
+        Debug.Log(ReplaceTextSymbols("The quick brown fox jumps\nover the lazy dog", 2));
     }
 
     // Update is called once per frame
@@ -93,6 +99,33 @@ public class GameManager : MonoBehaviour
             Power += addedEnergy;
         screen.SetBatteryText(power);
         screen.UpdateScreenText(correctText, correctnessList.ToArray(), Power);
+    }
+
+    private string ReplaceTextSymbols(string originalText, int glitchAmount)
+    {
+        char[] delimiterChars = {' ', '\n', '\t' };
+        string[] words = originalText.Split(delimiterChars);
+        for(int i = 0; i < glitchAmount; i++)
+        {
+            int numGlitches = Random.Range(minGlitchAmount, maxGlitchAmount);
+            for(int j = 0 ; j < numGlitches; j++)
+            {
+                int glitchIndex = Random.Range(0, words.Length);
+                string glitchWord = words[glitchIndex];
+                int letterIndex = Random.Range(0, glitchWord.Length);
+                if(letterIndex != glitchWord.Length)
+                    words[glitchIndex] = glitchWord.Substring(0, letterIndex) + glitchingCharacters[Random.Range(0, glitchingCharacters.Count)]
+                                                + glitchWord.Substring(letterIndex + 1);
+                else
+                    words[glitchIndex] = glitchWord.Substring(0, letterIndex) + glitchingCharacters[Random.Range(0, glitchingCharacters.Count)];
+            }
+        }
+        string finalText = "";
+        for(int i = 0; i < words.Length - 1; i++)
+        {
+            finalText += words[i] + " ";
+        }
+        return finalText + words[words.Length - 1];
     }
 
     private IEnumerator DrainBattery(AnimationCurve amount, float interval)
